1b2e6c6659f611f87e976b38bb138cae1c6126ae
camilla <camilla.gausland@gmail.com>
2020-03-09 14:02:11 +0100

    formatting

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import data from './data.json'
-import { Artist } from './Artist'
-// import DotsSvg from '/project-music-releases/code
+
 
 export const Album = (props) => {
     const {name} = props
@@ -22,36 +21,3 @@ export const Album = (props) => {
       </div>
 )
 }
-
-
-/* export const Album = (props) => {
-    return (
-      <div className="Card">
-          <div className="Symbols">
-              <div className="Like"><img src="heart.svg" alt="heart cion"/></div>
-              <div className="Play"><img src="play.svg" alt="play icon"/></div>
-              <div className="More"><img src="dots.svg" alt="dots icon"/></div>
-          </div>
-          <img src={props.image} alt="Album cover"></img>
-          <div className="Title">{props.title}</div>
-          {<div className="Artist">{props.name}</div>}
-
-      </div>
-)
-}
- */
-/* export const Wherever = (props) => { 
-  console.log(props) 
-  return (
-    <div>
-    
-      {props.item.artists.map(item => {
-        return (
-          <Artist key={item.id} item={item} />
-        
-        )
-      })}
-    </div>
-    
-  )
-} */
--- code/src/App.js
+++ code/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import data from './data.json'
 import { Album } from './Album'
-import { Artist } from './Artist'
-import { Wherever } from'./Album'
 
 
 export const App = () => {
@@ -25,23 +23,3 @@ export const App = () => {
     </div>
   )
 }
-
-/* export const App = () => {
-  return (
-    <div>
-      <h1>New albums & singles</h1>
-      <div className="Page">
-        {data.albums.items.map((album) => {
-          return ( <Album 
-          image={album.images[1].url}
-          key={album.artists[0].name}      
-          name={album.artists[0].name}
-          title={album.name}
-          />
-         
-        )}
-        )}
-      </div>
-    </div>
-  )
-} */
